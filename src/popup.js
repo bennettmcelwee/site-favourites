a2f167c14f09756f7d766798a69eabb64a4b8973
Bennett McElwee <bennett@thunderguy.com>
2018-08-02 17:29:47 +1200

    Keep separate bookmarks for full domain and base domain
    And allow the user to choose which to use

--- src/popup.js
+++ src/popup.js
@@ -4,7 +4,9 @@
 const EXTENSION_NAME = 'Site Favourites';
 const EXTENSION_VERSION = '1.0'; // Can't read the manifest from here
 
-let domain = 'unknown'
+let fullDomain = 'unknown'
+let baseDomain = 'unknown'
+let currentDomain = 'unknown'
 
 initialise()
 return
@@ -12,15 +14,44 @@ return
 function initialise () {
     chrome.tabs.query({currentWindow: true, active: true}, tabs => {
         if (tabs[0]) {
-            domain = new URL(tabs[0].url).hostname
-            $('h1 .site').text(domain)
+            fullDomain = new URL(tabs[0].url).hostname
+            baseDomain = getBaseDomain(fullDomain)
+            setDomain(fullDomain)
+            if (fullDomain !== baseDomain) {
+                $('.sf-site-selector').addClass('enabled').on('click', onToggleDomain)
+            }
         }
-        getJsonFromStorage('favourites', []).then(favourites => {
-            favourites.forEach(addToFavouritesMenu)
-            $('.sf-add').on('click', onAdd)
-            $('.sf-favourites').on('click', '.sf-remove', onRemove)
-            $('.sf-favourites').on('click', '.sf-link', onLink)
-        })
+        $('.sf-add').on('click', onAdd)
+        $('.sf-favourites').on('click', '.sf-remove', onRemove)
+        $('.sf-favourites').on('click', '.sf-link', onLink)
+    })
+}
+
+// Given a full domain (e.g. yats.solnet.co.nz), return the base (e.g. solnet.co.nz)
+function getBaseDomain(fullDomain) {
+    let matches
+    if (matches = fullDomain.match(/([^.]+\.[^.]{2,4}\.[^.]{2})$/)) {
+        return matches[0]
+    }
+    else if (matches = fullDomain.match(/([^.]+\.[^.]{2,})$/)) {
+        return matches[0]
+    }
+    else {
+        return fullDomain
+    }
+}
+
+function onToggleDomain(event) {
+    event.preventDefault()
+    setDomain(currentDomain === fullDomain ? baseDomain : fullDomain)
+}
+
+function setDomain(domain) {
+    currentDomain = domain
+    $('h1 .sf-site').text(currentDomain)
+    getJsonFromStorage('favourites', []).then(favourites => {
+        clearFavouritesMenu()
+        favourites.forEach(addToFavouritesMenu)
     })
 }
 
@@ -43,8 +74,8 @@ function onLink(event) {
 
 function getJsonFromStorage (subkey, fallback) {
     return new Promise((resolve,reject) => {
-        const key = `${domain}.${subkey}`
-        chrome.storage.local.get(key, values => {
+        const key = `${currentDomain}.${subkey}`
+        chrome.storage.sync.get(key, values => {
                 if (chrome.runtime.lastError) {
                     console.log(`${EXTENSION_NAME} failed to read ${key} from extension storage`, chrome.runtime.lastError)
                     resolve(fallback)
@@ -58,8 +89,8 @@ function getJsonFromStorage (subkey, fallback) {
 
 function saveJsonToStorage (subkey, value) {
     return new Promise((resolve,reject) => {
-        const key = `${domain}.${subkey}`
-        chrome.storage.local.set({[key]: value}, () => {
+        const key = `${currentDomain}.${subkey}`
+        chrome.storage.sync.set({[key]: value}, () => {
                 if (chrome.runtime.lastError) {
                     console.log(`${EXTENSION_NAME} failed to save ${key} to extension storage`, chrome.runtime.lastError)
                     reject()
@@ -88,8 +119,12 @@ function addToFavourites ({title, url} = {}) {
     }
 }
 
+function clearFavouritesMenu() {
+    $('.sf-favourites').empty()
+}
+
 function addToFavouritesMenu({title, url}) {
-    $('.sf-favourites').append(`<li><a href="#" class="sf-remove">&times;</a><a class="sf-link" href="${url}">${title}</a>`)
+    $('.sf-favourites').append(`<li><a href="#" class="sf-remove">&times;</a><a class="sf-link" href="${url}" title="${url}">${title}</a>`)
 }
 
 function removeFromFavourites ({element}) {
